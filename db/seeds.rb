118e6a030581e1cfa2f98c93bd1d249cc7647338
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-02 21:03:27 +0200

    secure v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user_lunaire = User.new(email: "toto@gmail.com", password: "vegeta", password_confirmation: "vegeta")
-user_lunaire.save
-
-user_space = User.new(email: "toto01@gmail.com", password: "codo", password_confirmation: "codo")
-user_space.save
-
-user_cookie = User.new(email: "toto02@gmail.com", password: "koko", password_confirmation: "koko")
-user_cookie.save
-
-user_sauce = User.new(email: "toto03@gmail.com", password: "songoku", password_confirmation: "songoku")
-user_sauce.save
-
-user_whatever = User.new(email: "toto04@gmail.com", password: "kakarot", password_confirmation: "kakarot")
-user_whatever.save
+User.new(email: "steven@gmail.com", password: "stevenv", password_confirmation: "stevenv")
