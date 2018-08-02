cefa2a39ea1a1f9fee0d57a96beb4eabe5a7bc9b
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-02 21:47:33 +0200

    push l

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'bcrypt'
+
 User.new(email: "steven@gmail.com", password: "stevenv", password_confirmation: "stevenv")
